587a99d1c6ab080a581349a352c435451344a487
ANKUSH\ankush <ankushraizada@gmail.com>
2016-11-27 12:44:02 -0600

    Resolving conflicts

--- src/com/aos/lab3/Process.java
+++ src/com/aos/lab3/Process.java
@@ -1,14 +1,14 @@
 package com.aos.lab3;
 
 import java.net.InetAddress;
+import java.util.Iterator;
 import java.util.Random;
-
+import java.lang.reflect.Field;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Process {
 
-	private static final int INITIAL_SLEEP_TIME = 6000;
 	private static Logger logger = LogManager.getLogger(Process.class);
 	private static Random rand = new Random();
 	private static int labelValue = rand.nextInt(9) + 1;
@@ -19,34 +19,69 @@ public class Process {
 			ConfigParser parser = new ConfigParser();
 			Config config = parser.getConfig();
 
+			String version = System.getProperty("version");
+
 			String hostname = InetAddress.getLocalHost().getHostName();
 			nodeId = Integer.valueOf(System.getProperty("nodeId"));
 			logger.info("Hostname:{} NodeId:{} Label value:{}", hostname, nodeId, labelValue);
 			Node node = config.getNodes().get(nodeId);
-
-			// starting servers
+			
+			//starting servers
 			Server server = new Server(nodeId, labelValue, node.getPort(), config);
 			Client client = new Client(hostname, labelValue, config, nodeId);
 			server.setClientHandler(client);
-
+			
 			Thread clientThread = new Thread(client, "client-thread");
 			Thread serverThread = new Thread(server, "server-thread");
 
 			clientThread.start();
 			serverThread.start();
-
-			// Wait for other nodes to come up
-			Thread.sleep(INITIAL_SLEEP_TIME);
-			logger.info("Sleeping for {}", INITIAL_SLEEP_TIME);
-
-			ICheckpointRequestHandler checkpointHandler = new CheckpointRequestHandler(client);
-			IRecoveryRequestHandler recoveryHandler = new RecoveryRequestHandler(client);
-			RequestingCandidate rc = new RequestingCandidate(config, nodeId, client, checkpointHandler,
-					recoveryHandler);
-			rc.start();
+			
+			//we sleep little before starting operations
+			Thread.sleep(10000);
+			
+			//Iterating through the list of operations
+			
+			/*Iterator itr = config.operations.iterator();
+			Operation opr= null;
+			while(itr.hasNext()){
+				opr = (Operation) itr.next();
+				if (opr.getNodeId() == nodeId && opr.getType().equals(OperationType.CHECKPOINT)) {
+					ICheckpointRequestHandler icrh = new CheckpointRequestHandler(client);
+					RequestingCandidate rc = new RequestingCandidate(config, nodeId, client, icrh);
+					server.setCheckpointHandler(icrh);
+					rc.requestCheckpoint();
+				}
+				else if(opr.getNodeId() == nodeId && opr.getType().equals(OperationType.RECOVERY)) {
+					IRecoveryRequestHandler irh = new RecoveryRequestHandler(client);
+					RequestingCandidate rc = new RequestingCandidate(config, nodeId, client, irh);
+					server.setRecovereHandler(irh);
+					rc.requestRecovery();
+				}
+			}*/
+			
 		} catch (Exception e) {
 			logger.error("Exception in Process", e);
 		}
 
 	}
+
+	// private static void writeOutputToFile(String hostname) throws IOException
+	// {
+	// BufferedWriter writer = new BufferedWriter(new FileWriter(hostname + "_"
+	// + nodeId + ".txt"));
+	// writer.write("Label value: " + labelValue);
+	// writer.write("\nOutput value: " + ServerWorker.getResult());
+	// writer.close();
+	// }
+
+	private static Integer getNodeId(String hostname, Config config) {
+		for (Node node : config.getNodes()) {
+			if (node.getHostname().equals(hostname))
+				return node.getNodeId();
+		}
+		logger.error("Unable to find nodeId for hostname: {} in the list", hostname);
+		return null;
+	}
+
 }
--- src/com/aos/lab3/RequestingCandidate.java
+++ src/com/aos/lab3/RequestingCandidate.java
@@ -3,6 +3,9 @@ package com.aos.lab3;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.Iterator;
+import java.util.Set;
+
 public class RequestingCandidate {
 
 	private static final Logger logger = LogManager.getLogger(RequestingCandidate.class);
@@ -10,90 +13,96 @@ public class RequestingCandidate {
 	private Config config;
 	private Integer nodeId;
 	private Client client;
-	private IRecoveryRequestHandler recoveryHandler;
-	private ICheckpointRequestHandler checkpointHandler;
-	private Integer counter = 0;
-
-	RequestingCandidate(Config config, Integer nodeId, Client client, ICheckpointRequestHandler checkpointHandler,
-			IRecoveryRequestHandler recoveryHandler) {
+	private IRecoveryRequestHandler iRecovereHandler;
+	private ICheckpointRequestHandler iCheckpointHandler;
+	
+	RequestingCandidate(Config config, Integer nodeId, Client client, ICheckpointRequestHandler iCheckpointHandler) {
 		this.config = config;
 		this.nodeId = nodeId;
 		this.client = client;
-		this.checkpointHandler = checkpointHandler;
-		this.recoveryHandler = recoveryHandler;
+		this.iCheckpointHandler = iCheckpointHandler;
+		this.initiator initiator;
 	}
 
-	public void start() {
-
-		 int appCount=1,ctrlCount=1;
-		 long timeToSend=0;
-		
-		
-		 while(ctrlCount<=config.getNoOfOperations() &&
-		 appCount<=config.getNoOfMsgs()){
-		 timeToSend = System.currentTimeMillis();
-		 if(!checkpointHandler.isRunning() && !recoveryHandler.isRunning()){ 
-			 //if no operation is currently running in
-		 //system then only send app msgs
-		 while(timeToSend<=(timeToSend+getExpoRandom(config.getMinInstanceDelay()))){
-		 appCount++;
-		 Thread.sleep(config.getMinSendDelay()); //sleep before sending
-		 Set<Integer> cohorts = config.getNodeIdVsNeighbors().get(nodeId)
-		 for(int i=0;i<.size();i++){
-			 Message msg = new Message(nodeId, , msgType)
-			 client.sendMsg(msg);
-		 }
-		 application msg
-		
-		 //send application message
-		
-		 timeToSend=System.currentTimeMillis();
-		 }
-		 }
-		 Iterator<Operation> it = config.getOperationsList().iterator();
-		 if(nodeId==it.node){
-		 //send checkpoint / rec req and carry out whole check/recov procedure
-		
-		 }
-		 else{
-		 while (true){
-		 Thread.sleep(10);
-		 if(//get opComplete msg from initiator)
-		 break;
-		 }
-		 it.next();
-		 }
-		
-		 }
-		 while(appCount<=config.getNoOfMsgs()){
-		 //just send application msgs
-		 }
-		 while(ctrlCount<=config.getNoOfOperations()){
-		 //perform operations with some instance delay
-		 }
-		
-		 //lets send checkpoint request to all and setting myself as true
-		 client.tentativeCheckpoint = true;
-		 iCheckpointHandler.spreadTheWord(config, nodeId);
-		
-
-		
+	RequestingCandidate(Config config2, Integer nodeId2, Client client2, IRecoveryRequestHandler iRecovereHandler) {
+		// TODO Auto-generated constructor stub
+		this.config = config2;
+		this.nodeId = nodeId2;
+		this.client = client2;
+		this.iRecovereHandler = iRecovereHandler;
 	}
 
 	public void requestCheckpoint() throws InterruptedException {
 
+		
+
+		//commenting Nikhil's code for now
+		/*int appCount=0,ctrlCount=0;
+		long timeToSend=0;
+
+
+		while(ctrlCount<=config.getNoOfOperations() && ctrlCount<=config.getNoOfMsgs()){
+			timeToSend = System.currentTimeMillis();
+			if(!isOpearationRunning){									//if no operation is currently running in system then only send app msgs
+				while(timeToSend<=(timeToSend+getExpoRandom(config.getMinInstanceDelay()))){
+					appCount++;
+					Thread.sleep(config.getMinSendDelay());				//sleep before sending application msg
+
+					//send application message
+
+					timeToSend=System.currentTimeMillis();
+				}
+			}
+			Iterator<Operation> it = config.getOperationsList().iterator();
+			if(nodeId==it.node){
+				//send checkpoint / rec req and carry out whole check/recov procedure
+
+ 			}
+ 			else{
+				while (true){
+					Thread.sleep(10);
+					if(//get opComplete msg from initiator)
+						break;
+				}
+				it.next();
+			}
+
+		}
+		while(ctrlCount<=config.getNoOfMsgs()){
+				//just send application msgs
+		}
+		while(ctrlCount<=config.getNoOfOperations()){
+			//perform operations with some instance delay
+		}*/
+		
+		//lets send checkpoint request to all and setting myself as true		
+		
+		//setting this only if its initiator
+		if(nodeId == initiator)	
+			client.tentativeCheckpoint = true;
+		
+		iCheckpointHandler.spreadTheWord(config, nodeId);
+		
+
+	}
+	
+	public void requestRecovery(){
+		
+		//reverting to old state
+		iRecovereHandler.revert();
+		iRecovereHandler.askOthersForRollback(config, nodeId);
+		
 	}
 
-	public void requestRecovery() {
-		// reverting to old state
-		recoveryHandler.revert();
-		recoveryHandler.askOthersForRollback(config, nodeId);
+//Commenting Nikhil's code for now
+/*	private static int getExpoRandom(int mean) {
 
-	}
-
-	private static int getExpoRandom(int mean) {
 		double temp = Math.random();
 		double exp = -(Math.log(temp) * mean);
+
 		return (int) exp;
-	}
+
+	}*/
+	
+
 }
